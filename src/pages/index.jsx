c01835005bd75ac244468be18237fd865d77e684
leah <theloadingpage@gmail.com>
2022-11-03 21:02:11 +0000

    update meta url

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -153,7 +153,7 @@ export default function Home({ episodes }) {
           property="og:description"
           content="A podcast by three south londoners: Reuben, Zach and Kehinde."
         />
-        <meta property="og:url" content="https://inbc-podcast.vercel.app/" />
+        <meta property="og:url" content="https://inbcp.vercel.app/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="It's Not A Book Club Podcast" />
         <meta
@@ -161,11 +161,7 @@ export default function Home({ episodes }) {
           content="https://storage.buzzsprout.com/variants/bZS7K9cwXTvz5m5ijveFFdS1/b49cbe86cb411762753e730c58953bb88ad958a9d657212c074729b6f04e5463"
         />
         <meta name="twitter:card" content="summary" />
-        <meta property="twitter:domain" content="inbc-podcast.vercel.app" />
-        <meta
-          property="twitter:url"
-          content="https://inbc-podcast.vercel.app/"
-        />
+        <meta property="twitter:url" content="https://inbcp.vercel.app/" />
         <meta name="twitter:title" content="It's Not A Book Club Podcast" />
         <meta
           name="twitter:description"
